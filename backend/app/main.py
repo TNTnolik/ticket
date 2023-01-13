70e45b531ce8d8b737277e9d5aba1274bb5a5692
TNTnolik <tntnolik@gmail.com>
2023-01-13 18:43:29 +1000

    sql

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,29 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.exc import IntegrityError
+from .db.service import init_models, get_session, get_tickets, add_ticket
+from .db.schemas import Ticket
+import asyncio
 
-from backend.app.db.service import init_models
-from .db.service import init_models
+
+app = FastAPI()
+
+@app.get("/tickets", response_model=list[Ticket])
+async def r_get_tickets(session: AsyncSession = Depends(get_session)):
+    tickets = await get_tickets(session)
+    return [Ticket(title=t.title, description=t.description) for t in tickets]
+
+
+@app.post("/tickets/add")
+async def r_add_ticket(ticket: Ticket, session: AsyncSession = Depends(get_session)):
+    new_ticket = add_ticket(session = session, title = ticket.title, description = ticket.description)
+    try:
+        await session.commit()
+        return new_ticket
+    except IntegrityError as ex:
+        await session.rollback()
+
+
+@app.get("/init")
+def init():
+    asyncio.run(init_models())

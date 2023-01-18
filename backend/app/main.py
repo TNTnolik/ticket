a8bee1659685cf3adea26150287e0d5895d1c87f
TNTnolik <tntnolik@gmail.com>
2023-01-19 01:06:15 +1000

    .

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.ext.asyncio import AsyncSession
+from fastapi import Depends, FastAPI
 from sqlalchemy.exc import IntegrityError
-from db.service import init_models, get_session, get_tickets, add_ticket
+from sqlalchemy.ext.asyncio import AsyncSession
+
 from db.schemas import Ticket
+from db.service import add_ticket, get_session, get_tickets
 
 
 app = FastAPI()
@@ -10,17 +11,18 @@ app = FastAPI()
 
 @app.get("/tickets", response_model=list[Ticket])
 async def r_get_tickets(session: AsyncSession = Depends(get_session)):
+    """Get tickets"""
     tickets = await get_tickets(session)
     return [Ticket(title=t.title, description=t.description) for t in tickets]
 
 
 @app.post("/tickets/add")
 async def r_add_ticket(ticket: Ticket, session: AsyncSession = Depends(get_session)):
-    new_ticket = add_ticket(session = session, title = ticket.title, description = ticket.description)
+    """Create ticket"""
+    new_ticket = add_ticket(
+        session=session, title=ticket.title, description=ticket.description)
     try:
         await session.commit()
         return new_ticket
     except IntegrityError as ex:
         await session.rollback()
-
-
